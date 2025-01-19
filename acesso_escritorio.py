965ebf4c689a7f3e499e634423cff3a8070ff445
Gilmar <gilmar.jesus@gmail.com>
2025-01-19 14:48:11 -0300

    Abordagem onde valido se o acesso é permitido primeiro e caso contrario será negado.

--- acesso_escritorio.py
+++ acesso_escritorio.py
@@ -1,6 +1,6 @@
 acesso = float(input("Digite a hora atual: "))
 
-if acesso <8 or acesso >18:
-    print("Acesso negado!")
-else:
+if  8 <= acesso < 18:
     print("Acesso permitido!")
+else:
+    print("Acesso negado!")
